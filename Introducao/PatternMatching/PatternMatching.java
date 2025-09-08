0468972e577d566f7ede80da814943347b21dd42
Vinicius <viniipvp@gmail.com>
2025-09-08 15:20:51 -0300

    Pattern Matching

--- Introducao/PatternMatching/PatternMatching.java
+++ Introducao/PatternMatching/PatternMatching.java
@@ -1,5 +1,17 @@
 package Introducao.PatternMatching;
 
 public class PatternMatching {
-    
+     public static void main(String[] args) {
+        imprimeSeForString("Java");
+        imprimeSeForString(false);
+     }
+
+     static void imprimeSeForString(Object o){
+        if(o instanceof String s){
+            System.out.println(s+" É String");
+        }
+        else{
+            System.out.println("Operacao inválida");
+        }
+     }
 }
