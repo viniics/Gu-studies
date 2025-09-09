6a92d7524cb7eab54154668e3d4616ec9fe64732
Vinícius <106555191+viniics@users.noreply.github.com>
2025-09-09 13:48:56 -0300

    Correct File

--- br/com/gubee/concurrency/src/VirtualThreadPrinting.java
+++ br/com/gubee/concurrency/src/VirtualThreadPrinting.java
@@ -2,11 +2,7 @@ package br.com.gubee.concurrency.src;
 
 public class VirtualThreadPrinting {
     public static void main(String[] args) throws InterruptedException {
-
-        for (int i = 0; i < 1000; i++) {
-            Thread vThread = Thread.ofVirtual().start(() -> {
-                System.out.println("Ola" + Thread.currentThread());
-            });
-        }
+        Thread thread = Thread.ofVirtual().start(() -> System.out.println("Hello"));
+        thread.join();
     }
 }
