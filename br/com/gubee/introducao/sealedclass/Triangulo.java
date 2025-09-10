5c8f6cc8646cffc1431ba9451e17608801d35da8
Vinicius <viniipvp@gmail.com>
2025-09-10 14:40:46 -0300

    remove

--- br/com/gubee/introducao/sealedclass/Triangulo.java
+++ br/com/gubee/introducao/sealedclass/Triangulo.java
@@ -1,9 +1,9 @@
 package br.com.gubee.introducao.sealedclass;
 
 //Não deve permitir.
-public record Triangulo() implements Quadrilatero {
-
-    @Override
+public record Triangulo() //implements Quadrilatero {
+{
+    //@Override
     public double calcularArea() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'calcularArea'");
